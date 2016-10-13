b4cf1a694b667a5b8d60c966ab35632581968bb2
Will Taylor <w1ll100@hotmail.co.uk>
2016-10-13 15:58:51 +0100

    More set up for simulation

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ import networkx as nx
 print("Initialized")
 
 G=nx.DiGraph()
-
+G.add_edge(0,1)
+G.add_edge(0,2)
+G.add_edge(0,3)
+G.add_edge(0,4)
+G.add_edge(0,5)
+for i in range(len(G.node)):
+	G.node[i]['mutant']=False
 graphSim=Simulator()
 graphSim.loadGraphStructure(G)
 graphSim.runSim(100)
--- simulator.py
+++ simulator.py
@@ -2,10 +2,10 @@ import networkx as nx
 import random
 
 class Simulator():
-	def __init__(self):
+	def __init__(self,graph=None):
 		print("Simulator instance created")
 		print(self)
-		self.graphStructure=None
+		self.graphStructure=graph
 		self.runSim(123)
 
 	def loadGraphStructure(self, graph):
@@ -13,7 +13,13 @@ class Simulator():
 
 	def runSim(self, iterations):
 		if self.graphStructure!=None:
-			simGraph=self.graphStructure
+
+			for i in range(iterations):
+				#Reset graph to base state
+				simGraph=self.graphStructure
+				mutantStart=random.randint(0,len(simGraph.node)-1)
+				simGraph.node[mutantStart]['mutant']=True
+
 			print(iterations)
 		else:
 			print("Failed to run sim: No graph loaded")
