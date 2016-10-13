377b6339b7c8eb2d26249b860d845ab2308fa535
Will Taylor <w1ll100@hotmail.co.uk>
2016-10-13 16:30:18 +0100

    Basic working prototype of simulation complete

--- main.py
+++ main.py
@@ -4,13 +4,11 @@ import networkx as nx
 print("Initialized")
 
 G=nx.DiGraph()
-G.add_edge(0,1)
-G.add_edge(0,2)
-G.add_edge(0,3)
-G.add_edge(0,4)
-G.add_edge(0,5)
+for i in range(500):
+	G.add_edge(0,i)
+	G.add_edge(i,0)
 for i in range(len(G.node)):
 	G.node[i]['mutant']=False
 graphSim=Simulator()
 graphSim.loadGraphStructure(G)
-graphSim.runSim(100)
+graphSim.runSim(20,1.2)
--- simulator.py
+++ simulator.py
@@ -6,20 +6,44 @@ class Simulator():
 		print("Simulator instance created")
 		print(self)
 		self.graphStructure=graph
-		self.runSim(123)
 
 	def loadGraphStructure(self, graph):
 		self.graphStructure=graph
+		if self.graphStructure==None:
+			print("Warning: loadGraphStructure called with None as argument, current graph in Simulator is None")
 
-	def runSim(self, iterations):
+	def runSim(self, trials,fitness=1.1):
 		if self.graphStructure!=None:
-
-			for i in range(iterations):
+			for i in range(trials):
 				#Reset graph to base state
-				simGraph=self.graphStructure
-				mutantStart=random.randint(0,len(simGraph.node)-1)
+				simGraph=nx.DiGraph(self.graphStructure)
+				numNodes=len(simGraph.node)
+				mutantStart=random.randint(0,numNodes-1)
 				simGraph.node[mutantStart]['mutant']=True
+				numMutants=1
+				numNonMutants=numNodes-1
+				iterations=0
+				while numMutants!=0 and numNonMutants!=0:
+					#Pick node at random, proportional to fitness
+					#Pick random neighbour of node
+					#If different, copy first node mutant status to second node
 
-			print(iterations)
+					#atm just selecting at random, change this to take fitness into account asap
+					nodeReproducing=random.randint(0,numNodes-1)
+
+					#If the reproducing node has at least one neighbour, choose one at random (uniform probability)
+					if len(simGraph.neighbors(nodeReproducing))>0:
+						nodeDying=random.sample(simGraph.neighbors(nodeReproducing),1)[0]
+					
+					if simGraph.node[nodeReproducing]['mutant']!=simGraph.node[nodeDying]['mutant']:
+						simGraph.node[nodeDying]['mutant']=simGraph.node[nodeReproducing]['mutant']
+						if simGraph.node[nodeReproducing]['mutant']==True:
+							numMutants+=1
+							numNonMutants-=1
+						else:
+							numMutants-=1
+							numNonMutants+=1
+					iterations+=1
+				print(iterations,numMutants,numNonMutants)
 		else:
 			print("Failed to run sim: No graph loaded")
