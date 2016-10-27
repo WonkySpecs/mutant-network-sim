4669c5607994c5b9c55f577cb6ca3e2320130c9f
Will Taylor <w1ll100@hotmail.co.uk>
2016-10-27 18:00:35 +0100

    Added urchin graph for test

--- main.py
+++ main.py
@@ -4,27 +4,38 @@ import time
 
 print("Initialized")
 G=nx.Graph()
-nodes=300
+nodes=100
 numTrials=30
 
 #Cycle
-# for i in range(nodes-1):
-# 	G.add_edge(i,i+1)
-# G.add_edge(0,nodes-1)
+for i in range(nodes-1):
+	G.add_edge(i,i+1)
+G.add_edge(0,nodes-1)
 
 #Clique
 #G=nx.complete_graph(nodes)
 
-G=nx.Graph()
+#Clique by hand (doesnt seem to be much if any different, suspect its basically the same thing)
+# G=nx.Graph()
+# for i in range(nodes-1):
+# 	for j in range(i+1,nodes):
+# 		G.add_edge(i,j)
 
-for i in range(nodes-1):
-	for j in range(i+1,nodes):
-		G.add_edge(i,j)
+#Urchin
+G=nx.Graph()
+if nodes%2==0:
+	for c in range(nodes//2-1):
+		for c2 in range(c+1,nodes//2):
+			G.add_edge(c,c2)
+	for n in range(nodes//2):
+		G.add_edge(c,n+nodes//2)
+
+
 for i in range(len(G.node)):
 	G.node[i]['mutant']=False
 startTime=time.time()
 graphSim=Simulator()
 graphSim.loadGraphStructure(G)
-graphSim.runSim(numTrials,1.2)
+graphSim.runSim(numTrials,2)
 totTime=time.time()-startTime
 print(str(numTrials)+ " trials ran in " + str(totTime) + ", average trial was " + str(totTime/numTrials))
