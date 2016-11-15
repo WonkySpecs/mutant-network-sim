a165a9bb7907da5bbd75887eb9dd56a4b601a0de
Will Taylor <w1ll100@hotmail.co.uk>
2016-11-15 14:07:15 +0000

    Beginning to include more graphs from nx

--- main.py
+++ main.py
@@ -8,49 +8,53 @@ print("Initialized")
 def buildGraph(graphType, nodes):
 	G = nx.Graph()
 
-	if graphType == "complete":
-		G = nx.complete_graph(nodes)
+	if graphType in nx.generators.classic.__all__:
+		#Very cool but sketchy - not all of these methods only take a 'nodes' argument, probably need to hard code a few more things here. Works for wheel graph at least
+		G = getattr(nx.generators.classic, graphType)(nodes)
+	else:
+		if graphType == "complete":
+			G = nx.complete_graph(nodes)
 
-	elif graphType == "cycle":
-		for i in range(nodes-1):
-			G.add_edge(i,i+1)
-		G.add_edge(0,nodes-1)
+		elif graphType == "cycle":
+			for i in range(nodes-1):
+				G.add_edge(i,i+1)
+			G.add_edge(0,nodes-1)
 
-	elif graphType == "urchin":
-		G = nx.Graph()
-		n = nodes//2
-		if nodes%2==0:
-			for c in range(n-1):
-				for c2 in range(c+1,n):
-					G.add_edge(c,c2)
-			for m in range(n):
-				G.add_edge(m,m+n)
-		else:
-			print("nodes must be even for an Urchin graph")
-			sys.exit()
+		elif graphType == "urchin":
+			G = nx.Graph()
+			n = nodes//2
+			if nodes%2==0:
+				for c in range(n-1):
+					for c2 in range(c+1,n):
+						G.add_edge(c,c2)
+				for m in range(n):
+					G.add_edge(m,m+n)
+			else:
+				print("nodes must be even for an Urchin graph")
+				sys.exit()
 
-	elif graphType == "clique-wheel":
-		n = nodes//2
-		G = nx.complete_graph(n)
-		if nodes%2==0:
-			for m in range(n):
-				G.add_edge(m, m + n)
-			for w in range(n - 1):
-				G.add_edge(w+n ,w+n+1)
-			G.add_edge(n, nodes - 1)
-		else:
-			print("nodes must be even for a clique wheel graph")
-			sys.exit()
+		elif graphType == "clique-wheel":
+			n = nodes//2
+			G = nx.complete_graph(n)
+			if nodes%2==0:
+				for m in range(n):
+					G.add_edge(m, m + n)
+				for w in range(n - 1):
+					G.add_edge(w+n ,w+n+1)
+				G.add_edge(n, nodes - 1)
+			else:
+				print("nodes must be even for a clique wheel graph")
+				sys.exit()
 
 	for i in range(len(G.node)):
-		G.node[i]['mutant']=False
+		G.node[i]['mutant'] = False
 	return G
 
-nodes=120
-numTrials=10
-graphType = "clique-wheel"
+nodes=50
+numTrials=100
+graphType = "wheel_graph"
 
-G = buildGraph(nodes, graphType)
+G = nx.Graph(buildGraph(graphType, nodes))
 
 #startTime=time.time()
 graphSim=Simulator(False)
@@ -64,7 +68,7 @@ minDiff = 1
 r = 1.1
 for i in range(metaNumTrials):
 	expectedF = 1 - 1/r
-	fixated, extinct, iterations = graphSim.runSim(numTrials, r, nodes-1)
+	fixated, extinct, iterations = graphSim.runSim(numTrials, r, 0)
 	fixation += fixated
 	actualF = float(fixated)/numTrials
 	diff = abs(actualF - expectedF)
