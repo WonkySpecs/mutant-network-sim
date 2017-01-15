e9d6cb1e616b5e8ec540d87ccb37d932073ab4c0
Will Taylor <w1ll100@hotmail.co.uk>
2017-01-15 15:12:51 +0000

    Deleted some unused code

--- main.py
+++ main.py
@@ -129,27 +129,3 @@ if __name__ == "__main__":
 	fixated, extinct, iterations = graphSim.runSim(numTrials, 5, 70)
 
 	print("{} fixated, {} extinct, {} fixation\nTook {} seconds".format(fixated, extinct, fixated/(fixated+extinct), time.time() - startTime))
-
-#For running many batches of trials
-# metaNumTrials = 500
-# totalDiff = 0
-# fixation = 0
-# maxDiff = 0
-# minDiff = 1
-# r = 1.1
-# for i in range(metaNumTrials):
-# 	expectedF = 1 - 1/r
-# 	fixated, extinct, iterations = graphSim.runSim(numTrials, r, 0)
-# 	fixation += fixated
-# 	actualF = float(fixated)/numTrials
-# 	diff = abs(actualF - expectedF)
-# 	if diff > maxDiff:
-# 		maxDiff = diff
-# 	if diff < minDiff:
-# 		minDiff = diff
-# 	totalDiff += diff
-# 	print(i)
-# averageDiff = float(totalDiff)/metaNumTrials
-# print("Average diff = {}, maxDiff = {}, minDiff = {}, total fixation rate was {}".format(averageDiff, maxDiff, minDiff, float(fixation)/(metaNumTrials*numTrials)))
-# totTime=time.time()-startTime
-# print(str(numTrials)+ " trials ran in " + str(totTime) + ", average trial was " + str(totTime/numTrials))
