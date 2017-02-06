ccbeec43d93c80290f8092df6a919b8ac6e66222
Will Taylor <w1ll100@hotmail.co.uk>
2017-02-06 17:57:41 +0000

    Code for checking if expected graph parameters passed to graphs

--- graph_classes/GraphClass_cycle.py
+++ graph_classes/GraphClass_cycle.py
@@ -1,7 +1,27 @@
 import networkx as nx
 
 class GraphClass_cycle():
-	def buildGraph(nodes):
+	def checkParamsValid(params):
+		shouldHaveParams = [i for i in metadata[arguments].keys()]
+		for k in params.keys():
+			if k in shouldHaveParams:
+				shouldHaveParams.remove(k)
+			else:
+				print("buildGraph given a parameter called {} which it should not have".format(k))
+				return
+
+		if shouldHaveParams:
+			print("Necessary parameter(s) missing from buildGraph for {} graph: {}".format(metadata['name'], shouldHaveParams))
+			return -1
+		else:
+			return 1
+
+	def buildGraph(parameters):
+		if checkParamsValid(parameters) == -1:
+			return -1
+
+		nodes = parameters['nodes']
+
 		G = nx.Graph()
 
 		for i in range(nodes - 1):
@@ -13,7 +33,7 @@ class GraphClass_cycle():
 	metadata = {
 	"name"				: "cycle",
 	"display_name"		: "Cycle",
-	"argument_names"	: ["nodes",],
+	"arguments"			: {"nodes" : {'type' : 'int'}},
 	"description"		: ( "Basic graph class - each node has 2 neighbours"
 							"\nnodes parameter is the number of nodes")
 	}
--- graph_classes/GraphClass_urchin.py
+++ graph_classes/GraphClass_urchin.py
@@ -1,9 +1,31 @@
 import networkx as nx
 
 class GraphClass_urchin:
-	def buildGraph(nodes):
+	def checkParamsValid(params):
+		shouldHaveParams = [i for i in metadata[arguments].keys()]
+		for k in params.keys():
+			if k in shouldHaveParams:
+				shouldHaveParams.remove(k)
+			else:
+				print("buildGraph given a parameter called {} which it should not have".format(k))
+				return
+
+		if shouldHaveParams:
+			print("Necessary parameter(s) missing from buildGraph for {} graph: {}".format(metadata['name'], shouldHaveParams))
+			return -1
+		else:
+			return 1
+
+	def buildGraph(parameters):
+		if checkParamsValid(parameters) == -1:
+			return -1
+
+		nodes = parameters['nodes']
+
 		G = nx.Graph()
+
 		n = nodes // 2
+
 		if nodes % 2 == 0:
 			for c in range(n - 1):
 				for c2 in range(c + 1, n):
@@ -19,7 +41,7 @@ class GraphClass_urchin:
 	metadata = {
 		"name"				: "urchin",
 		"display_name"		: "Urchin",
-		"argument_names"	: ["nodes",],
+		"arguments"			: {"nodes" : {'type' : 'int'}},
 		"description"		: ( "nodes 0-(n/2 -1) form a clique,"
 								"(n/2 - [n-1]) are an independent"
 								"set, linked 1-1 with the nodes"
