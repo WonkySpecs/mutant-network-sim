a2e037037f8e93b28bf487b6d2faa64db82867e7
Will Taylor <w1ll100@hotmail.co.uk>
2017-04-27 23:58:38 +0100

    Improvements to frequency histograms

--- prettyoutput.py
+++ prettyoutput.py
@@ -6,25 +6,28 @@ import numpy as np
 def freqHistogram(frequencyDict):
 	maxIterKey = max(frequencyDict.keys())
 	maxIterValue = max(frequencyDict.values())
+	minIterKey = min(frequencyDict.keys())
+
+	keyDiff = maxIterKey - minIterKey
 
 	sortIter = sorted(frequencyDict)
 
-	paddedIter = [0 for i in range(maxIterKey + 1)]
+	paddedIter = [0 for i in range(minIterKey, maxIterKey + 1)]
 
 	for k,v in frequencyDict.items():
-		paddedIter[k] = v
+		paddedIter[k - minIterKey] = v
 
 	numElements = len(paddedIter)
 	fig, ax = plt.subplots()
 
-	plt.bar(range(numElements), paddedIter, align="center", facecolor = "green", alpha = 0.8)
+	plt.bar(range(minIterKey, maxIterKey + 1), paddedIter, align = "center", facecolor = "green", alpha = 0.8)
 
-	plt.xticks(range(0, numElements, numElements//20), [(numElements//20) * i for i in range(numElements)])
+	plt.xlim([minIterKey - 3, maxIterKey + 3])
+
+	plt.xticks(range(minIterKey, maxIterKey, numElements//20), [minIterKey + ((numElements//20) * i) for i in range(numElements)], rotation = 80)
 
 	plt.xlabel("Iterations")
 	plt.ylabel("Frequency")
-	plt.title("Number of iterations until fixation/extinction")
-	
-	plt.xlim([-1, maxIterKey + 1])
+	plt.title("Number of iterations until fixation")
 
 	plt.show()
